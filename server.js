633384e92988fa0419f1084b9ba7a260a7cb06ec
BhogavarapuChandrakala <146967411+BhogavarapuChandrakala@users.noreply.github.com>
2024-10-18 11:23:06 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+const app = express();
+
+// MongoDB Connection (without deprecated options)
+mongoose.connect('mongodb://localhost/urlShortener')
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// Express settings
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find();
+  res.render('index', { shortUrls: shortUrls });
+});
+
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect('/');
+});
+
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  await shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+// Use a different port if 4400 is unavailable
+const PORT = process.env.PORT || 4240;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
